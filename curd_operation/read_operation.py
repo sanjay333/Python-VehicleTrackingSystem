5cef12e420315d49f25a17f78f80a18e395c403c
unknown <bishtsanjay333@gmail.com>
2017-12-25 20:39:03 +0530

    add modules

--- curd_operation/read_operation.py
+++ curd_operation/read_operation.py
@@ -1,23 +1,24 @@
+#!/usr/bin/env python
+def getval()
+ import MySQLdb
+ cnx = MySQLdb.connect(user='root', passwd= 'mother',db='vehicle_database')
+ cursor = cnx.cursor()
 
-import mysql.connector 
+ query = ("SELECT PlateNo, Name, AddressId FROM owner_detail")
+ cursor.execute(query)
 
-cnx = mysql.connector.connect(user='root', password= 'mother',database='vehicle_database') #create connection with mysql sever
-cursor = cnx.cursor()
-
-query = ("SELECT PlateNo, Name, AddressId FROM owner_detail")
-cursor.execute(query)
-
-# print the rows
-#for row in cursor :
-#    print(row[1])
+ # print the rows
+ #for row in cursor :
+ #    print(row[1])
  
-result_set= cursor.fetchall()
-for row in result_set:
-    print(row[0])
+ result_set= cursor.fetchall()
+ #for row in result_set:
+ #    print(row)
 
-#cursor.execute(queryfor ( PlateNo, Name, AddressId) in cursor:
-    #print {}.format(cursor)
-    #print {}{}{}.format(PlateNo,Name,AddressId)
+ #cursor.execute(queryfor ( PlateNo, Name, AddressId) in cursor:
+ #print {}.format(cursor)
+ #print {}{}{}.format(PlateNo,Name,AddressId)
 
-cursor.close()
-cnx.close()
+ cursor.close()
+ cnx.close()
+ return result_set
